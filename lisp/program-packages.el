aaa64bbe2290d0036074737c5d1902224bfa40d7
askeySnip <lz.askey@gmail.com>
2022-12-05 19:29:52 +0800

    remove lsp-java support
    Signed-off-by: askeySnip <lz.askey@gmail.com>

--- lisp/program-packages.el
+++ lisp/program-packages.el
@@ -74,10 +74,6 @@
   (c++-mode . lsp-deferred)
   (c++-mode . c-toggle-hungry-state))
 
-(use-package lsp-java
-  :ensure t
-  :config (add-hook 'java-mode-hook 'lsp))
-
 (use-package treemacs
   :ensure t
   :defer t
